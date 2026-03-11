d7f7a17965e07557646242686c77a7fc2518bd8c
sanikaghule16 <sanikaghule4@gmail.com>
2026-03-11 13:27:19 +0530

    Create Student.cs Model File

---
+++ Student.cs
@@ -0,0 +1,16 @@
+// Model File
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvCDtabase.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+    }
+}
